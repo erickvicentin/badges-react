e1bc46f113bdefd7aa8de968c62d9918ec4a6e29
Erick Vicentin <erickvicentin14@hotmail.com>
2020-02-01 07:12:31 -0300

    se llama a la pagina BADGES como principal

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 const container = document.getElementById("app");
 
 //ReactDOM.render( que , donde);
 
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
