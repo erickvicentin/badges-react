d1b743363d6559536ea45bdcfe123a95f440819b
Erick Vicentin <erickvicentin14@hotmail.com>
2020-02-01 04:34:23 -0300

    add page BadgeNew (first page)

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,11 +1,37 @@
 import React from "react";
+
+import "./styles/BadgeNew.css";
 import NavBar from "../components/NavBar";
+import header from "../images/badge-header.svg";
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
   render() {
     return (
       <div>
         <NavBar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName="Erick"
+                lastName="Vicentin"
+                jobTitle="Front-End Developer"
+                twitter="nitnecivTB"
+                avatarUrl= "https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14046.jpg"
+              />
+            </div>
+
+            <div className="col-6">
+              <BadgeForm />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
