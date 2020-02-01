a1147f7dc51d069924dd0a8e8b40c2fc3b618bb3
Erick Vicentin <erickvicentin14@hotmail.com>
2020-02-01 06:36:55 -0300

    se levanta el state de cada campo del formulario a la pagina principal

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
-  handlerChange = e => {
-    console.log({
-      name: e.target.name,
-      value: e.target.value
-    });
-  };
+  state = {};
+
+  //handlerChange = e => {
+  //  console.log({
+  //    name: e.target.name,
+  //    value: e.target.value
+  //this.setState({
+  //  [e.target.name]: e.target.value
+  //});
+  //};
 
   handlerClick = e => {
     console.log("Se clickeo el boton");
+  };
+
+  handlerSubmit = e => {
+    e.preventDefault();
+    console.log("form was sumbitted");
   };
 
   render() {
@@ -17,22 +26,63 @@ class BadgeForm extends React.Component {
       <div>
         <h1> Nuevo Participante </h1>
 
-        <form>
+        <form onSubmit={this.handlerSubmit}>
           <div className="form-group">
             <label> First Name</label>
             <input
-              onChange={this.handlerChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               name="firstName"
+              value={this.state.firstName}
             />
           </div>
 
-          <button
-            type="button"
-            onClick={this.handlerClick}
-            className="btn btn-primary"
-          >
+          <div className="form-group">
+            <label> Last Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label> Email </label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="email"
+              value={this.state.email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label> Job Title</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.state.jobTitle}
+            />
+          </div>
+
+          <div className="form-group">
+            <label> Twitter</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.state.twitter}
+            />
+          </div>
+
+          <button onClick={this.handlerClick} className="btn btn-primary">
             SAVE
           </button>
         </form>
