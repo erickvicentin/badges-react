8466ef0fa64939aa1babbe42ec476bfb4b632fe7
Erick Vicentin <erickvicentin14@hotmail.com>
2020-02-02 10:07:51 -0300

    modificacion en barra de navegacion, links a paginas

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../images/logocuadrado.png";
-import './styles/NavBar.css';
+import "./styles/NavBar.css";
 
 class NavBar extends React.Component {
   render() {
     return (
-        <div className="Navbar">
+      <div className="Navbar">
         <div className="container-fluid">
-            <a className="Navbar__brand" href="/">
-                <img className="Badge_logo" src={logo} alt="Logo" />
-                <span className="font-weight-light">CNEISI</span>
-                <span className="font-weight-bold">Conference</span>
-            </a>
+          <Link className="Navbar__brand" to="/">
+            <img className="Badge_logo" src={logo} alt="Logo" />
+            <span className="font-weight-light">CNEISI</span>
+            <span className="font-weight-bold">
+              2020 - Facultad Regional Resistencia
+            </span>
+          </Link>
         </div>
-        </div>
-    )
+      </div>
+    );
   }
 }
 
