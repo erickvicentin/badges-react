19aef9a367ee9632550b06a576343258d1a4abff
Erick Vicentin <erickvicentin14@hotmail.com>
2020-02-02 11:39:03 -0300

    personalizacion de pagina NotFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,14 @@
 import React from "react";
+import notFound from "../images/404.png";
+import "./styles/NotFound.css";
 
 function NotFound() {
-  return <h1>404! Not Found</h1>;
+  return (
+    <div className="NotFound_page">
+      <img src={notFound} alt="Error 404! Page not found" />
+      <h1>Page not found</h1>;
+    </div>
+  );
 }
 
 export default NotFound;
