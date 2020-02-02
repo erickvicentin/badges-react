62f65b560560d0ad84b1ef508ecd442eb2d91914
Erick Vicentin <erickvicentin14@hotmail.com>
2020-02-02 10:09:20 -0300

    se agregan los links en pantallas para redireccionar a la que corresponda.

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles/BadgesList.css";
 
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length == 0) {
+      return (
+        <div>
+          <h3>No se encontraron insignias</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Crear nueva insignia
+          </Link>
+        </div>
+      );
+    }
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import "./styles/BadgeNew.css";
-import NavBar from "../components/NavBar";
 import header from "../images/badge-header.svg";
 import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
@@ -29,7 +28,6 @@ class BadgeNew extends React.Component {
   render() {
     return (
       <div>
-        <NavBar />
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="Logo" />
         </div>
